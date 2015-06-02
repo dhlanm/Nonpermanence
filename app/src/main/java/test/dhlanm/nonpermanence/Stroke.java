0f70f8e4bbd5fa0513a4fa39c9dbaef25e077933
addtheletters <bzhang.2003@gmail.com>
2015-06-02 13:09:45 -0400

    android probably has something similar built in
    but whatever

--- app/src/main/java/test/dhlanm/nonpermanence/Stroke.java
+++ app/src/main/java/test/dhlanm/nonpermanence/Stroke.java
@@ -3,16 +3,31 @@ package test.dhlanm.nonpermanence;
 import android.graphics.Color;
 import android.graphics.Point;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ben on 6/2/2015.
  */
 public class Stroke {
     boolean black; // true = black, false = white
-    float size; // this could be an int probably?
+    int size;
 
-    public Stroke(boolean black, float s){
+    ArrayList<Point> points;
+
+    public Stroke(boolean black, int s){
+        this(black, s, new ArrayList<Point>());
+    }
+
+    public Stroke(boolean black, int s, ArrayList<Point> pts){
         this.black = black;
         this.size = s;
+        this.points = pts;
     }
+
+    public void addPoint(Point p){
+        points.add(p);
+    }
+
+    
 
 }
